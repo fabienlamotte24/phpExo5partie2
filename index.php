401ddbe4534e893eb833f3342e49911497a01338
fabienlamotte24 <fabien.lamotte00@outlook.fr>
2018-07-26 09:33:20 +0200

    exo 5 partie 2 php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8"  />
+    <title>Exercice 4</title>
+  </head>
+  <body>
+    <p>
+<?php
+  $gender = 'homme';
+  if($gender == 'homme'){
+    echo 'C\'est un développeur !!!';
+  } else {
+    echo 'C\'est une développeuse !!!';
+  }
+ ?>
+    </p>
+  </body>
+</html>
